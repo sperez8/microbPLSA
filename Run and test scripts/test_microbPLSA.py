cf688b583d7f5eb2de78dc9e7b491450a7f6710d
sperez <sperez@shaban.mi.microbiology.ubc.ca>
2013-11-04 17:25:58 -0800

    Can print out topic labels. Optional 'sampling' parameter to analyze only a subset of the data.

--- Run and test scripts/test_microbPLSA.py
+++ Run and test scripts/test_microbPLSA.py
@@ -5,7 +5,9 @@ sys.path.append('/Users/sperez/git/microbPLSA')
 import microbplsa
 
 f = '/Users/sperez/Documents/workspace/myprojects/myplsa/otu_table.txt'
-m = microbplsa.MicrobPLSA(f)
+m = microbplsa.MicrobPLSA(f,sampling = True)
 
-m.runplsa(10, verbatim = True, maxiter= 100)
+model = m.runplsa(5, verbatim = True, maxiter= 100)
 m.saveresults('test')
+
+print 'Topic Labels', model.topic_labels(None)
