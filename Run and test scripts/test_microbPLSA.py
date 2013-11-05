05a8100752ca64a4a1e40ddd1875eff125cc6dbd
sperez8 <karatezeus21@gmail.com>
2013-11-04 16:29:59 -0800

    finished draft of running file. every works as it did before.

--- Run and test scripts/test_microbPLSA.py
+++ Run and test scripts/test_microbPLSA.py
@@ -4,5 +4,8 @@ sys.path.append('/Users/sperez/git/microbPLSA')
 
 import microbplsa
 
-f = 'otu_table.txt'
+f = '/Users/sperez/Documents/workspace/myprojects/myplsa/otu_table.txt'
 m = microbplsa.MicrobPLSA(f)
+
+m.runplsa(10, verbatim = True, maxiter= 100)
+m.saveresults('test')
