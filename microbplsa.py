6600df593f6f551a549f027c8c4df012cc1ff427
sperez8 <karatezeus21@gmail.com>
2013-11-04 16:40:36 -0800

    changed order of functions. made importdata() a staticmethod

--- microbplsa.py
+++ microbplsa.py
@@ -18,21 +18,44 @@ class MicrobPLSA():
     def __init__(self, file):
         self.file = file
     
-    def importdata(self, filename):
-        '''imports the date from filename and saves it in numpy array format'''
-        file = open(filename,'r')
-        table = file.read().splitlines() #read file and split by lines
-        columns = table[1].split('\t')  #read column names which are split by tabs
-        columns.pop(0)
-        otus = []
-        datamatrix = np.ndarray((len(table)-2,len(columns)))
-        for i in range(2,len(table)):
-            row = table[i].split('\t')
-            otus.append(row.pop(0)) 
-            datamatrix[i-2] = (row)
-    
-        #return datamatrix, otus
-        return columns, datamatrix, np.array([int(x) for x in otus])
+    def runplsa(self, topic_number, maxiter, verbatim = True):
+        '''runs plsa on sample data in filename'''
+        columns, datamatrix, otus = self.importdata(self.file)
+        Z = topic_number #number of topics
+        if verbatim: 
+            print '\nData in matrix form:\n', datamatrix
+            print len(otus), 'Otus:',otus
+            print len(columns), 'Sample names:', columns
+            print Z, 'topics.'
+            
+        plsa = pLSA()
+        plsa.debug = verbatim
+        plsa.random_init(Z, len(otus), len(columns))
+        print "\n Running PLSA.\n"
+        plsa.train(datamatrix, Z, maxiter)   #runs plsa!
+        self.model = plsa
+        return self.model
+        
+    def saveresults(self, filename = 'results'):
+        ''' functions saves plsa probabilities into a .csv file'''
+        filename = self.formatfile(filename)
+        f = open(filename,'w')
+        writer = csv.writer(f)
+        p_z,p_w_z,p_d_z = self.model.get_model()
+        
+        writer.writerow(['p_z', p_z.shape])
+        for value in p_z:
+            writer.writerow(value)
+        writer.writerow(['p_d_z', p_d_z.shape])
+        for value in p_d_z:
+            writer.writerow(value)    
+        writer.writerow(['p_w_z', p_w_z.shape])
+        for value in p_w_z:
+            writer.writerow(value)
+            
+        f.close()
+        
+        return None
     
     def svd(self, columns, datamatrix, otus):
         '''performs svd on data. NEEDS TESTING'''
@@ -59,44 +82,6 @@ class MicrobPLSA():
         #new_data = np.dot(np.dot(U, np.diag(s)), np.conjugate(V))
         #print new_data
     
-    def runplsa(self, topic_number, maxiter, verbatim = True):
-        '''runs plsa on sample data in filename'''
-        columns, datamatrix, otus = self.importdata(self.file)
-        Z = topic_number #number of topics
-        if verbatim: 
-            print '\nData in matrix form:\n', datamatrix
-            print len(otus), 'Otus:',otus
-            print len(columns), 'Sample names:', columns
-            print Z, 'topics.'
-            
-        plsa = pLSA()
-        plsa.debug = verbatim
-        plsa.random_init(Z, len(otus), len(columns))
-        print "\n Running PLSA.\n"
-        plsa.train(datamatrix, Z, maxiter)   #runs plsa!
-        self.model = plsa
-        return self.model
-        
-    def saveresults(self, filename = 'results'):
-        ''' functions saves plsa probabilities into a .csv file'''
-        filename = formatfilename(filename)
-        f = open(filename,'w')
-        writer = csv.writer(f)
-        p_z,p_w_z,p_d_z = self.model.get_model()
-        
-        writer.writerow(['p_z', p_z.shape])
-        for value in p_z:
-            writer.writerow(value)
-        writer.writerow(['p_d_z', p_d_z.shape])
-        for value in p_d_z:
-            writer.writerow(value)    
-        writer.writerow(['p_w_z', p_w_z.shape])
-        for value in p_w_z:
-            writer.writerow(value)
-            
-        f.close()
-        
-        return None
     
     @staticmethod
     def formatfile(filename):
@@ -108,7 +93,22 @@ class MicrobPLSA():
             filename = filename +'.csv'
         return filename
     
-
+    @staticmethod
+    def importdata(self, filename):
+        '''imports the date from filename and saves it in numpy array format'''
+        file = open(filename,'r')
+        table = file.read().splitlines() #read file and split by lines
+        columns = table[1].split('\t')  #read column names which are split by tabs
+        columns.pop(0)
+        otus = []
+        datamatrix = np.ndarray((len(table)-2,len(columns)))
+        for i in range(2,len(table)):
+            row = table[i].split('\t')
+            otus.append(row.pop(0)) 
+            datamatrix[i-2] = (row)
+    
+        #return datamatrix, otus
+        return columns, datamatrix, np.array([int(x) for x in otus])
 
 
 
