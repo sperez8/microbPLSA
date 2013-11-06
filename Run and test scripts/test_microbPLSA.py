0b7095c4c60a41307fed374174e2fc701cf13524
sperez8 <karatezeus21@gmail.com>
2013-11-05 18:22:16 -0800

    debugging why plsa errors when using biom data... also 'sampling' non functional for biom data yet.

--- Run and test scripts/test_microbPLSA.py
+++ Run and test scripts/test_microbPLSA.py
@@ -4,8 +4,10 @@ sys.path.append('/Users/sperez/git/microbPLSA')
 
 import microbplsa
 
+#f = '/Users/sperez/Documents/PLSAfun/EMPL data/study_1037_closed_reference_otu_table.biom'
 f = '/Users/sperez/Documents/workspace/myprojects/myplsa/otu_table.txt'
-m = microbplsa.MicrobPLSA(f,sampling = True)
+
+m = microbplsa.MicrobPLSA(f,sampling = False)
 
 model = m.runplsa(5, verbatim = True, maxiter= 100)
 m.saveresults('test')
---
+++ utilities.py
@@ -0,0 +1,101 @@
+'''
+Created on 3/11/2013
+
+author: sperez8
+
+contains a bunch of different utilities to read EMP data
+
+'''
+import numpy as np
+import json
+from pprint import pprint
+import sys
+
+
+
+def find_otu_name(id):
+    '''returns the OTU name for an OTU id'''
+    id = str(id)
+    tax_file = '/Users/sperez/Documents/PLSAfun/gg_otus_4feb2011/taxonomies/greengenes_tax.txt'
+    tax_table = open(tax_file,'r')
+    
+    for line in tax_table:
+        if id in line[0:len(id)]:
+            return line.replace(id, '')
+
+def extract_data(file,sampling):
+    if file[-4:]=='.txt':
+        return import_tab_file(file, sampling)
+    elif file[-5:]=='.biom':
+        return import_biom_file(file, sampling)
+    else: 
+        print "File name:", file
+        print "Incorrect file extension."
+        sys.exit()
+            
+def import_biom_file(f,sampling):
+    '''reads a .biom file and extracts OTU count data
+    using the json library since .biom is a json file.
+    See biom-format.org/documentation/versions/biom-1.0.html for more info'''
+    json_data=open(f)
+    data = json.load(json_data)
+    #pprint(data) #use this command to print the whole file
+    json_data.close()
+    
+    shape = data['shape']
+    if sampling:
+        datamatrix = np.zeros((501, shape[1]))
+    else: 
+        datamatrix = np.zeros((shape[0], shape[1]))
+                 
+    data_type = data['matrix_type']  #can either be 'sparse' or 'dense'
+
+    
+    if data_type == 'sparse':
+        row_otu = 0
+        row = 0
+        for otu, sample, count in data['data']:
+            if otu != row_otu:
+                row_otu = otu
+                row += 1
+            if sampling and row > 500: break #doesn't work/untested
+            else: datamatrix[otu][sample] = count
+    if data_type == 'dense': #NOT TESTED YET
+        row = 0
+        for otu_counts in data['data']:
+            datamatrix[row] = otu_counts    
+            row += 1   
+            if sampling and row > 500: break 
+    
+        
+    return [], datamatrix, []
+
+def import_tab_file(filename, sampling):
+    '''imports the date from filename and saves it in numpy array format'''
+    file = open(filename,'r')
+    table = file.read().splitlines() #read file and split by lines
+    columns = table[1].split('\t')  #read column names which are split by tabs
+    columns.pop(0)
+    otus = []
+    datamatrix = np.ndarray((len(table)-2,len(columns)))
+    if sampling and len(table)>1000:
+        last_line = 500
+    else: last_line = len(table)
+    for i in range(2,last_line):
+        row = table[i].split('\t')
+        otus.append(row.pop(0)) 
+        datamatrix[i-2] = (row)
+
+    #return datamatrix, otus
+    return columns, datamatrix, np.array([int(x) for x in otus])
+
+
+
+
+#Testing find_otu_name()
+import time
+t0 = time.time()
+id = 89440
+print find_otu_name(id)
+print time.time() - t0
+
