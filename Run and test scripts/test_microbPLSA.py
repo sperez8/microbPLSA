6df59596f1ba26bad9a5c4fa3c5546b2892a6cd7
sperez <sperez@shaban.mi.microbiology.ubc.ca>
2013-11-15 13:10:51 -0800

    Changed maxiter

--- Run and test scripts/test_microbPLSA.py
+++ Run and test scripts/test_microbPLSA.py
@@ -1,6 +1,9 @@
-import sys
+import sys, os
+from time import time
 
-sys.path.append('/Users/sperez/git/microbPLSA') 
+_cur_dir = os.path.dirname(os.path.realpath(__file__))
+_root_dir = os.path.dirname(_cur_dir)
+sys.path.insert(0, _root_dir)
 
 import microbplsa
 
@@ -9,7 +12,9 @@ f = '/Users/sperez/Documents/PLSAfun/EMPL data/study_1037_closed_reference_otu_t
 
 m = microbplsa.MicrobPLSA(f,sampling = False)
 
-model = m.runplsa(5, verbatim = True, maxiter= 100)
+t0 = time()
+model = m.runplsa(5, verbatim = True)
 m.saveresults('results')
 
-print 'Topic Labels', model.topic_labels(None)
+print '\n\n\n Topic Labels', model.topic_labels(None)
+print 'Time for analysis:', int(time()-t0)
