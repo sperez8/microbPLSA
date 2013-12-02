f299f38f9fe0caaea8a99cec8253e48fc3d5432d
sperez8 <karatezeus21@gmail.com>
2013-12-02 13:44:42 -0800

    Added a variable called LEVELS to open_otu_map to specify how many levels we need to name an OTU

--- MicrobProcessor/Runscripts/topic_labels.py
+++ MicrobProcessor/Runscripts/topic_labels.py
@@ -9,7 +9,11 @@ _cur_dir = os.path.dirname(os.path.realpath(__file__))
 _root_dir = os.path.dirname(_cur_dir)
 sys.path.insert(0, _root_dir)
 
-import microbplsa
+import microbplsa, time
+
+
+t0 = time.time()
+
 
 N=5
 biom_data = '/Users/sperez/Documents/PLSAfun/EMPL data/study_1037_closed_reference_otu_table.biom'
@@ -18,16 +22,23 @@ f = "/Users/sperez/git/microbPLSA/MicrobProcessor/Results/results_"
 m = microbplsa.MicrobPLSA()
 m.open_otu_maps(biom_data)
 
-end = '_topics_26Nov.txt'
-for z in range(2,7):
+t0 = time.time()
+print time.time()-t0
+
+
+
+end = '_topics_29Nov.txt'
+for z in range(2,8):
     print '\nz = ', z
     labels = m.topic_OTUS(f+str(z)+end,N)
     for label in labels:
         print label
     
-end = '_topics_27Nov.txt'
-for z in range(7,13):
+end = '_topics_30Nov.txt'
+for z in range(8,12):
     print '\nz = ', z
     labels = m.topic_OTUS(f+str(z)+end,N)
     for label in labels:
         print label
+        
+print time.time()-t0
