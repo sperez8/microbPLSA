67100fa911081dc22b66dd5c5316aa5c0c6235b7
sperez8 <karatezeus21@gmail.com>
2014-03-05 17:22:33 -0800

    got some kind of PCA to work. Need to make sure it does what I want it to do.

--- MicrobProcessor/Analysis/clustering.py
+++ MicrobProcessor/Analysis/clustering.py
@@ -4,7 +4,16 @@ Created on 22/01/2014
 author: sperez8
 
 Performs clustering on raw otu counts
+PCA stuff from http://scikit-learn.org/stable/auto_examples/decomposition/plot_pca_3d.html
 '''
+from sklearn.decomposition import PCA
+
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import pylab as pl
+from scipy import stats
+
+
 from scipy.cluster.vq import kmeans
 import os, sys
 
@@ -22,25 +31,50 @@ _root_dir = os.path.dirname(_cur_dir)
 sys.path.insert(0, _root_dir)
 import microbplsa
 
-study = '1037'
-z = 8
+study = '722'
+z = 22
+
+
 
 f = '/Users/sperez/git/microbPLSA/MicrobProcessor/Results/study_'+study +'_'
 end = '_topics_.txt'
 file = f+str(z)+end
 
+def makedendrogram(file):
+    m = microbplsa.MicrobPLSA()
+    plsa = m.open_model(file) #get model from the results file'
+    X = plsa.p_d_z
+    
+    Y = pdist(X, 'euclidean')
+    
+    Z = linkage(Y)
+    
+    D = dendrogram(Z)
+    print D['ivl']
+    show()
+    return None
 
-m = microbplsa.MicrobPLSA()
-plsa = m.open_model(file) #get model from the results file'
-X = plsa.p_d_z
 
-print X.shape
+def makePCA(file):
+    m = microbplsa.MicrobPLSA()
+    plsa = m.open_model(file) #get model from the results file'
+    X = plsa.p_d_z    
+    
+    
+    pca = PCA(n_components=2, whiten = True, )
+    pca.fit(X)
+    pca_score = pca.explained_variance_ratio_
+    V = pca.components_
+    print V.shape
+    print V
+    print pca_score* V.T
 
-Y = pdist(X, 'euclidean')
+    x_pca_axis, y_pca_axis = V
+    pcaplot = plt.scatter(x_pca_axis, y_pca_axis)
+    show()
 
-Z = linkage(Y)
-
-D = dendrogram(Z)
-print D['ivl']
-show()
-
+    return None
+    
+makePCA(file)
+    
+    
